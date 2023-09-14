2d8b58f3d561b06e004bb3f8ce86011777b6a6d6
Fabian <fabian.lillomartinez@gmail.com>
2023-09-14 00:11:26 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'curriculum',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
